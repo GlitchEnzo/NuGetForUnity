f8d651b75d889a2ff414ae3f25e17563f2621208
GlitchEnzo <mccarthp@gmail.com>
2015-11-05 16:58:13 -0800

    Created a placeholder Nuspec Editor
    It doesn't do anything yet, but it soon will.

---
+++ Assets/NuGet/NuspecEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DefaultAsset))]
+public class NuspecEditor : Editor
+{
+    //[MenuItem("Assets/Edit Nuspec")]
+    //static void EditMyType()
+    //{
+    //    //MyEditorWindow.Init(Selection.activeObject as MyType);
+    //}
+
+    //[MenuItem("Assets/Edit Nuspec", true)]
+    //static bool EditMyTypeValidate()
+    //{
+    //    Debug.Log(Selection.activeObject);
+    //    //return (Selection.activeObject.GetType() == typeof(MyType));
+    //    return false;
+    //}
+
+    public void OnEnable()
+    {
+        string assetPath = AssetDatabase.GetAssetPath(target);
+        Debug.Log("Custom Editor! " + assetPath);
+        if (target.name == "manifest")
+        {
+        }
+    }
+}
