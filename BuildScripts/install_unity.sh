22b742ca1070e4d986a044b759bebd4d165ccfd8
GlitchEnzo <mccarthp@gmail.com>
2017-11-07 21:07:53 -0500

    Simplified the Unity installer
    The paths were wrong and causing the install to fail.  They were fixed
    and the script was simplified to help reduce errors.

--- BuildScripts/install_unity.sh
+++ BuildScripts/install_unity.sh
@@ -4,25 +4,12 @@
 # Shown in this article: https://hackernoon.com/writing-an-open-source-unity-package-877bad3c8913
 
 # Get the URLs manually from: https://unity3d.com/get-unity/download/archive
-BASE_URL=http://netstorage.unity3d.com/unity
+#URL=https://download.unity3d.com/download_unity/649f48bbbf0f/MacEditorInstaller/Unity-5.4.1f1.pkg
+URL=https://download.unity3d.com/download_unity/e7947df39b5c/MacEditorInstaller/Unity-5.2.0f3.pkg
+PACKAGE=Unity-5.2.0f3.pkg
 
-# https://download.unity3d.com/download_unity/649f48bbbf0f/MacEditorInstaller/Unity-5.4.1f1.pkg
-#HASH=649f48bbbf0f
-#VERSION=5.4.1f1
+echo "Downloading from $URL: "
+curl -o `basename "$PACKAGE"` "$URL"
 
-# https://download.unity3d.com/download_unity/e7947df39b5c/MacEditorInstaller/Unity-5.2.0f3.pkg
-HASH=e7947df39b5c
-VERSION=5.2.0f3
-
-install() {
-  package=$1
-  url="$BASE_URL/$HASH/$package"
-
-  echo "Downloading from $url: "
-  curl -o `basename "$package"` "$url"
-
-  echo "Installing "`basename "$package"`
-  sudo installer -dumplog -package `basename "$package"` -target /
-}
-
-install "MacEditorInstaller/Unity-$VERSION.pkg"
+echo "Installing "`basename "$PACKAGE"`
+sudo installer -dumplog -package `basename "$PACKAGE"` -target /
