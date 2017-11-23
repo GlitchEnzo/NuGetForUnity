4ef049cf8b55e3458b6bd4ef6157cfc257406151
GlitchEnzo <mccarthp@gmail.com>
2017-11-22 21:29:22 -0500

    Set up the Chocolatey environment
    Install-ChocolateyPackage cannot be found, so the helper needs to set up
    the environment first.

--- BuildScripts/installUnity.ps1
+++ BuildScripts/installUnity.ps1
@@ -1,3 +1,8 @@
+# First set up the Chocolatey environment so that the Install commandlet is available
+# See here: https://stackoverflow.com/questions/35558911/why-is-the-uninstall-chocolateypackage-cmdlet-not-recognized
+&"C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1"
+
+# The direct URL to the Unity installer:
 # https://download.unity3d.com/download_unity/e7947df39b5c/Windows64EditorInstaller/UnitySetup64-5.2.0f3.exe
 
 $version = "5.2.0";
