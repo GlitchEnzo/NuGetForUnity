791175a635af4ce7ca6ba190ba69f9bf7a4bfcd8
Patrick McCarthy <mccarthp@gmail.com>
2017-11-22 19:00:55 -0500

    Updated version to 0.0.13 (#122)
    Version 0.0.12 was submitted to the Unity Asset Store, so any further
    changes should be under a new version number.

--- Assets/NuGet/Editor/NugetPreferences.cs
+++ Assets/NuGet/Editor/NugetPreferences.cs
@@ -11,7 +11,7 @@
         /// <summary>
         /// The current version of NuGet for Unity.
         /// </summary>
-        public const string NuGetForUnityVersion = "0.0.12";
+        public const string NuGetForUnityVersion = "0.0.13";
 
         /// <summary>
         /// The current position of the scroll bar in the GUI.
