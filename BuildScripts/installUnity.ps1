12349005d8366ddb0261d4e2a1583584b3121644
GlitchEnzo <mccarthp@gmail.com>
2018-02-13 19:38:29 -0500

    Switch to Unity 5.3.4p6
    This should be the lowest patch release that fixes the command line
    activation bug.

--- BuildScripts/installUnity.ps1
+++ BuildScripts/installUnity.ps1
@@ -8,7 +8,8 @@ function Write-Log
 # Get direct URLs to the Unity installers here:
 # https://unity3d.com/get-unity/download/archive
 #$url = "https://download.unity3d.com/download_unity/e7947df39b5c/Windows64EditorInstaller/UnitySetup64-5.2.0f3.exe";
-$url = "https://download.unity3d.com/download_unity/960ebf59018a/Windows64EditorInstaller/UnitySetup64-5.3.5f1.exe";
+$url = "https://beta.unity3d.com/download/9eb825cf2ca3/Windows64EditorInstaller/UnitySetup64-5.3.4p6.exe";
+#$url = "https://download.unity3d.com/download_unity/960ebf59018a/Windows64EditorInstaller/UnitySetup64-5.3.5f1.exe";
 
 Write-Log "Downloading the Unity installer from `n $url";
 
@@ -25,7 +26,8 @@ Write-Log "Finished downloading. Running Unity installer...";
 # /S = Performs a silent (no questions asked) install.
 # | Out-Null forces PowerShell to wait for the installer to finish before continuing
 #.\UnitySetup64-5.2.0f3.exe /S | Out-Null
-.\UnitySetup64-5.3.5f1.exe /S | Out-Null
+.\UnitySetup64-5.3.4p6.exe /S | Out-Null
+#.\UnitySetup64-5.3.5f1.exe /S | Out-Null
 
 Write-Log "Finished installing Unity.";
 
