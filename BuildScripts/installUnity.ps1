a7f273909d463a85dd86aae7cf08cd0963e0f95c
GlitchEnzo <mccarthp@gmail.com>
2018-02-13 19:27:14 -0500

    Updated to Unity 5.3.5
    Unity 5.2 was giving a false error during activation and failing. It was
    supposed fixed in a patch of 5.3.5, this installer version might not
    have the fix.
    
    Info here:
    https://answers.unity.com/questions/1171010/batchmode-license-activation-error.html

--- BuildScripts/installUnity.ps1
+++ BuildScripts/installUnity.ps1
@@ -7,7 +7,8 @@ function Write-Log
 
 # Get direct URLs to the Unity installers here:
 # https://unity3d.com/get-unity/download/archive
-$url = "https://download.unity3d.com/download_unity/e7947df39b5c/Windows64EditorInstaller/UnitySetup64-5.2.0f3.exe";
+#$url = "https://download.unity3d.com/download_unity/e7947df39b5c/Windows64EditorInstaller/UnitySetup64-5.2.0f3.exe";
+$url = "https://download.unity3d.com/download_unity/960ebf59018a/Windows64EditorInstaller/UnitySetup64-5.3.5f1.exe";
 
 Write-Log "Downloading the Unity installer from `n $url";
 
@@ -23,7 +24,8 @@ Write-Log "Finished downloading. Running Unity installer...";
 # https://docs.unity3d.com/Manual/InstallingUnity.html
 # /S = Performs a silent (no questions asked) install.
 # | Out-Null forces PowerShell to wait for the installer to finish before continuing
-.\UnitySetup64-5.2.0f3.exe /S | Out-Null
+#.\UnitySetup64-5.2.0f3.exe /S | Out-Null
+.\UnitySetup64-5.3.5f1.exe /S | Out-Null
 
 Write-Log "Finished installing Unity.";
 
