7075e037410683105043f101247b0fb357a97471
Josh Wittner <jowitt@microsoft.com>
2021-01-15 10:37:56 -0800

    Bump packager up to 2018.4

---
+++ Packager/Assets/Export.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+
+namespace NugetForUnity
+{
+    /// <summary>
+    /// Enables export of the package from Menus or the Cmd Line
+    /// </summary>
+    public static class Export
+    {
+        [MenuItem("Packager/Export", priority = -1)]
+        public static void Execute()
+        {
+            AssetDatabase.ExportPackage(
+                new string[] { "Assets/Nuget" },
+                "NugetForUnity.unitypackage",
+                ExportPackageOptions.Recurse);
+        }
+    }
+}
+
