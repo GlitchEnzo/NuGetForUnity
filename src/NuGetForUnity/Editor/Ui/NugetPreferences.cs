e258619a03f68dea4e0a9842a1eca924624efff0
JoC0de <53140583+JoC0de@users.noreply.github.com>
2023-10-21 14:53:20 +0200

    update version number to 4.0.0

--- src/NuGetForUnity/Editor/Ui/NugetPreferences.cs
+++ src/NuGetForUnity/Editor/Ui/NugetPreferences.cs
@@ -28,7 +28,7 @@ namespace NugetForUnity.Ui
         /// <summary>
         ///     The current version of NuGet for Unity.
         /// </summary>
-        public const string NuGetForUnityVersion = "3.1.3";
+        public const string NuGetForUnityVersion = "4.0.0";
 
         /// <summary>
         ///     The current position of the scroll bar in the GUI.
